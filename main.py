951c6eff9b7871b19936eaaa9fafbad0005bf609
gabikohen <gabokohen@gmail.com>
2022-05-01 16:05:38 -0300

    Initial commit python

---
+++ main.py
@@ -0,0 +1,24 @@
+# variables
+viriable = 24
+units = "hours"
+
+
+def days_to_units(num_of_days, custum_message):
+    print(f"{num_of_days} days are {35 * viriable} {units} ")
+    print(custum_message)
+
+
+days_to_units(60, "How is the calculations")
+days_to_units(60, "Look good!")
+
+
+
+# case 1
+print("20 days are " + str(50) + " seconds ")
+
+# case 2
+print(f"20 days are {20 * viriable } {units} ")
+print(f"35 days are { 35  * viriable } {units} ")
+print(f"100 days are { 100 * viriable } {units} ")
+print(f"180 days are { 180 * viriable  } {units} ")
+
